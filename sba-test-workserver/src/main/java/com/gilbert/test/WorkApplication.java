0926294f8701f138bfd4768b35d36c598422002d
houjinbiao <houjb1@lenovo.com>
2018-10-30 15:06:26 +0800

    demo for sba

---
+++ sba-test-workserver/src/main/java/com/gilbert/test/WorkApplication.java
@@ -0,0 +1,27 @@
+package com.gilbert.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@Configuration
+public class WorkApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+
+}
